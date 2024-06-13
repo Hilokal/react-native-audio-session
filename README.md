afd98281cf2537bf014ba7042fe75c632b5a991b
Jonathan Baudanza <jon@jonb.org>
2024-06-13 16:37:40 +0900

    gitpkg

---
+++ README.md
@@ -0,0 +1,130 @@
+# react-native-audio-session
+[![npm version](https://img.shields.io/npm/v/react-native-audio-session.svg?style=flat)](https://www.npmjs.com/package/react-native-audio-session)
+[![npm downloads](https://img.shields.io/npm/dm/react-native-audio-session.svg?style=flat)](https://www.npmjs.com/package/react-native-audio-session)
+
+A React Native module for handling category, category options and mode on the [`AVAudioSession`](https://developer.apple.com/documentation/avfoundation/avaudiosession?language=objc) sharedInstance on iOS.
+
+## Getting started
+
+`$ yarn add react-native-audio-session`
+
+and then
+
+`$ react-native link react-native-audio-session`
+
+## Usage
+
+```javascript
+import AudioSession from 'react-native-audio-session'
+
+// Set AVAudioSession active
+AudioSession.setActive(true)
+.then(() => { console.log('Success!') })
+.catch(error => { console.log('Error!') })
+
+// Set AVAudioSession category
+AudioSession.setCategory('Playback')
+.then(() => { console.log('Success!') })
+.catch(error => { console.log('Error!') })
+
+// Set AVAudioSession category and options
+AudioSession.setCategory('Playback', 'MixWithOthers')
+.then(() => { console.log('Success!') })
+.catch(error => { console.log('Error!') })
+
+// Set AVAudioSession mode
+AudioSession.setMode('VoiceChat')
+.then(() => { console.log('Success!') })
+.catch(error => { console.log('Error!') })
+
+// Set AVAudioSession category, mode and options
+AudioSession.setCategoryAndMode('Playback', 'VoiceChat', 'MixWithOthers')
+.then(() => { console.log('Success!') })
+.catch(error => { console.log('Error!') })
+
+// Get current AVAudioSession category
+AudioSession.currentCategory().then(category => {
+  console.log(category)
+})
+
+// Get current AVAudioSession options
+AudioSession.currentCategoryOptions().then(options => {
+  console.log(options)
+})
+
+// Get current AVAudioSession mode
+AudioSession.currentMode().then(mode => {
+  console.log(mode)
+})
+
+```
+
+## API
+
+### Modules
+#### AudioSession
+| Method Name | Params | Returns | Notes |
+|---|---|---|---|
+|setActive|`Bool`|`Promise<void>`|Set the current AVAudioSession as active [(Reference)](https://developer.apple.com/documentation/avfoundation/avaudiosession/1616627-setactive?language=objc).|
+|setCategory|`AudioCategory`, `AudioCategoryOptions` (nullable)|`Promise<void>`|Set the current AVAudioSession category [(Reference)](https://developer.apple.com/documentation/avfoundation/avaudiosession/1616442-setcategory?language=objc).|
+|setMode|`AudioMode`|`Promise<void>`|Set the current AVAudioSession mode [(Reference)](https://developer.apple.com/documentation/avfoundation/avaudiosession/1616614-setmode?language=objc).|
+|setCategoryAndMode|`AudioCategory`, `AudioMode`, `AudioCategoryOptions` (nullable)|`Promise<void>`|Set the current AVAudioSession category and mode [(Reference)](https://developer.apple.com/documentation/avfoundation/avaudiosession/1771734-setcategory?language=objc).|
+|currentCategory|-|`Promise<AudioCategory>`|Get the current AVAudioSession category [(Reference)](https://developer.apple.com/documentation/avfoundation/avaudiosession/1616615-category?language=objc).|
+|currentOptions|-|`Promise<AudioCategoryOptions>`|Get the current AVAudioSession options [(Reference)](https://developer.apple.com/documentation/avfoundation/avaudiosession/1616503-categoryoptions?language=objc).|
+|currentMode|-|`Promise<AudioMode>`|Get the current AVAudioSession mode [(Reference)](https://developer.apple.com/documentation/avfoundation/avaudiosession/1616508-mode?language=objc).|
+
+### Params
+#### AudioCategory
+The `AudioCategory` param to all methods. Corresponds to `AVAudioSessionCategory` in iOS. Read more: [Audio Session Category (developer.apple.com)](https://developer.apple.com/documentation/avfoundation/avaudiosessioncategory?language=objc)
+
+| Param | AVAudioSessionCategory |
+|---|---|
+|`Ambient`|`AVAudioSessionCategoryAmbient`|
+|`SoloAmbient`|`AVAudioSessionCategorySoloAmbient`|
+|`Playback`|`AVAudioSessionCategoryPlayback`|
+|`Record`|`AVAudioSessionCategoryRecord`|
+|`PlayAndRecord`|`AVAudioSessionCategoryPlayAndRecord`|
+|`MultiRoute`|`AVAudioSessionCategoryMultiRoute`|
+
+#### AudioCategoryOptions
+The `AudioCategoryOptions` param to all methods. Corresponds to `AVAudioSessionCategoryOptions` in iOS. Read more: [AVAudioSessionCategoryOptions (developer.apple.com)](https://developer.apple.com/documentation/avfoundation/avaudiosessioncategoryoptions?language=objc)
+
+| Param | AVAudioSessionCategoryOptions |
+|---|---|
+|`MixWithOthers`|`AVAudioSessionCategoryOptionMixWithOthers`|
+|`DuckOthers`|`AVAudioSessionCategoryOptionDuckOthers`|
+|`InterruptSpokenAudioAndMixWithOthers`|`AVAudioSessionCategoryOptionInterruptSpokenAudioAndMixWithOthers`|
+|`AllowBluetooth`|`AVAudioSessionCategoryOptionAllowBluetooth`|
+|`AllowBluetoothA2DP`|`AVAudioSessionCategoryOptionAllowBluetoothA2DP`|
+|`AllowAirPlay`|`AVAudioSessionCategoryOptionAllowAirPlay`|
+|`DefaultToSpeaker`|`AVAudioSessionCategoryOptionDefaultToSpeaker`|
+
+#### AudioMode
+The `AudioMode` param to all methods. Corresponds to `AVAudioSessionMode` in iOS. Read more: [Audio Session Mode (developer.apple.com)](https://developer.apple.com/documentation/avfoundation/avaudiosessionmode?language=objc)
+
+| Param | AVAudioSessionMode |
+|---|---|
+|`Default`|`AVAudioSessionModeDefault`|
+|`VoiceChat`|`AVAudioSessionModeVoiceChat`|
+|`VideoChat`|`AVAudioSessionModeVideoChat`|
+|`GameChat`|`AVAudioSessionModeGameChat`|
+|`VideoRecording`|`AVAudioSessionModeVideoRecording`|
+|`Measurement`|`AVAudioSessionModeMeasurement`|
+|`MoviePlayback`|`AVAudioSessionModeMoviePlayback`|
+|`SpokenAudio`|`AVAudioSessionModeSpokenAudio`|
+
+## Contributing
+
+If you find a bug or would like to request a new feature, just [open an issue](https://github.com/BonnierNews/react-native-audio-session/issues/new). You are also welcome to submit pull requests and contribute to the project.
+
+## License
+
+#### The MIT License (MIT)
+
+Copyright (c) 2018 Johan Kasperi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ ios/RNAudioSession.m
@@ -0,0 +1,182 @@
+//
+//  RNAudioSession.m
+//  RNAudioSession
+//
+//  Created by Johan Kasperi (DN) on 2018-03-02.
+//
+
+#import "RNAudioSession.h"
+#import <React/RCTLog.h>
+#import <AVFoundation/AVFoundation.h>
+
+@implementation RNAudioSession
+
+static NSDictionary *_categories;
+static NSDictionary *_options;
+static NSDictionary *_modes;
+
++ (void)initialize {
+    _categories = @{
+        @"Ambient": AVAudioSessionCategoryAmbient,
+        @"SoloAmbient": AVAudioSessionCategorySoloAmbient,
+        @"Playback": AVAudioSessionCategoryPlayback,
+        @"Record": AVAudioSessionCategoryRecord,
+        @"PlayAndRecord": AVAudioSessionCategoryPlayAndRecord,
+        @"MultiRoute": AVAudioSessionCategoryMultiRoute
+   };
+    _options = @{
+        @"MixWithOthers": @(AVAudioSessionCategoryOptionMixWithOthers),
+        @"DuckOthers": @(AVAudioSessionCategoryOptionDuckOthers),
+        @"InterruptSpokenAudioAndMixWithOthers": @(AVAudioSessionCategoryOptionInterruptSpokenAudioAndMixWithOthers),
+        @"AllowBluetooth": @(AVAudioSessionCategoryOptionAllowBluetooth),
+        @"AllowBluetoothA2DP": @(AVAudioSessionCategoryOptionAllowBluetoothA2DP),
+        @"AllowAirPlay": @(AVAudioSessionCategoryOptionAllowAirPlay),
+        @"DefaultToSpeaker": @(AVAudioSessionCategoryOptionDefaultToSpeaker)
+    };
+    _modes = @{
+        @"Default": AVAudioSessionModeDefault,
+        @"VoiceChat": AVAudioSessionModeVoiceChat,
+        @"VideoChat": AVAudioSessionModeVideoChat,
+        @"GameChat": AVAudioSessionModeGameChat,
+        @"VideoRecording": AVAudioSessionModeVideoRecording,
+        @"Measurement": AVAudioSessionModeMeasurement,
+        @"MoviePlayback": AVAudioSessionModeMoviePlayback,
+        @"SpokenAudio": AVAudioSessionModeSpokenAudio
+    };
+}
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(category:(RCTResponseSenderBlock)callback)
+{
+    NSString *cat = [AVAudioSession sharedInstance].category;
+    NSArray *temp = [_categories allKeysForObject:cat];
+    NSString *key = [temp lastObject];
+    if (key) {
+        callback(@[key]);
+    } else {
+        callback(@[]);
+    }
+}
+
+RCT_EXPORT_METHOD(options:(RCTResponseSenderBlock)callback)
+{
+    NSUInteger options = [AVAudioSession sharedInstance].categoryOptions;
+    NSArray *temp = [_options allKeysForObject:@(options)];
+    NSString *key = [temp lastObject];
+    if (key) {
+        callback(@[key]);
+    } else {
+        callback(@[]);
+    }
+}
+
+RCT_EXPORT_METHOD(mode:(RCTResponseSenderBlock)callback)
+{
+    NSString *mode = [AVAudioSession sharedInstance].mode;
+    NSArray *temp = [_modes allKeysForObject:mode];
+    NSString *key = [temp lastObject];
+    if (key) {
+        callback(@[key]);
+    } else {
+        callback(@[]);
+    }
+}
+
+RCT_EXPORT_METHOD(setActive:(BOOL)active resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+{
+    NSError *error = nil;
+    [[AVAudioSession sharedInstance] setActive:active withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:&error];
+    if (error) {
+        reject(@"setActive", @"Could not set active.", error);
+    } else {
+        resolve(@[]);
+    }
+}
+
+RCT_EXPORT_METHOD(setCategory:(NSString *)category options:(NSArray<NSString *> *)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+{
+    NSString* cat = _categories[category];
+    if (cat != nil && [[AVAudioSession sharedInstance].availableCategories containsObject:cat]) {
+        NSError *error = nil;
+        AVAudioSessionCategoryOptions categoryOptions = 0;
+
+        for (NSString *option in options) {
+            if (_options[option] != nil) {
+                categoryOptions |= [_options[option] integerValue];
+            }
+        }
+
+        [[AVAudioSession sharedInstance] setCategory:cat withOptions:categoryOptions error:&error];
+
+        if (error) {
+            reject(@"setCategory", @"Could not set category.", error);
+        } else {
+            resolve(@[]);
+        }
+    } else {
+        NSDictionary *userInfo = @{
+            NSLocalizedDescriptionKey: @"Could not set AVAudioSession category.",
+            NSLocalizedFailureReasonErrorKey: @"The given category is not supported on this device.",
+            NSLocalizedRecoverySuggestionErrorKey: @"Try another category."
+        };
+        NSError *error = [NSError errorWithDomain:@"RNAudioSession" code:-1 userInfo:userInfo];
+        reject(@"setCategory", @"Could not set category.", error);
+    }
+}
+
+RCT_EXPORT_METHOD(setMode:(NSString *)mode resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+{
+    NSString* mod = _modes[mode];
+    if (mod != nil && [[AVAudioSession sharedInstance].availableModes containsObject:mod]) {
+        NSError *error = nil;
+        [[AVAudioSession sharedInstance] setMode:mod error:&error];
+        if (error) {
+            reject(@"setMode", @"Could not set mode.", error);
+        } else {
+            resolve(@[]);
+        }
+    } else {
+        NSDictionary *userInfo = @{
+                                   NSLocalizedDescriptionKey: @"Could not set AVAudioSession mode.",
+                                   NSLocalizedFailureReasonErrorKey: @"The given mode is not supported on this device.",
+                                   NSLocalizedRecoverySuggestionErrorKey: @"Try another mode."
+                                   };
+        NSError *error = [NSError errorWithDomain:@"RNAudioSession" code:-1 userInfo:userInfo];
+        reject(@"setMode", @"Could not set mode.", error);
+    }
+}
+
+RCT_EXPORT_METHOD(setCategoryAndMode:(NSString *)category mode:(NSString *)mode options:(NSArray<NSString *> *)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+{
+    NSString* cat = _categories[category];
+    NSString* mod = _modes[mode];
+    if (cat != nil && mod != nil && _options[options] != nil && [[AVAudioSession sharedInstance].availableCategories containsObject:cat] && [[AVAudioSession sharedInstance].availableModes containsObject:mod]) {
+        NSError *error = nil;
+
+        AVAudioSessionCategoryOptions categoryOptions = 0;
+
+        for (NSString *option in options) {
+            if (_options[option] != nil) {
+                categoryOptions |= [_options[option] integerValue];
+            }
+        }
+
+        [[AVAudioSession sharedInstance] setCategory:cat mode:mod options:categoryOptions error:&error];
+        if (error) {
+            reject(@"setCategoryAndMode", @"Could not set category and mode.", error);
+        } else {
+            resolve(@[]);
+        }
+    } else {
+        NSDictionary *userInfo = @{
+                                   NSLocalizedDescriptionKey: @"Could not set AVAudioSession category and mode.",
+                                   NSLocalizedFailureReasonErrorKey: @"The given category or mode is not supported on this device.",
+                                   NSLocalizedRecoverySuggestionErrorKey: @"Try another category or mode."
+                                   };
+        NSError *error = [NSError errorWithDomain:@"RNAudioSession" code:-1 userInfo:userInfo];
+        reject(@"setCategoryAndMode", @"Could not set category and mode.", error);
+    }
+}
+
+@end
